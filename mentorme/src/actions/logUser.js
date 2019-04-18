3c1cf9b758d99445e4140190d6c615195f624fb3
dislersd <dislersd@gmail.com>
2019-04-18 10:11:09 -0700

    latest change

--- mentorme/src/actions/logUser.js
+++ mentorme/src/actions/logUser.js
@@ -3,21 +3,12 @@ import {LOGIN_START, LOGIN_SUCCESS, LOGIN_FAILURE} from '.';
 
 export const logUser = creds => dispatch => {
   dispatch({type: LOGIN_START});
-<<<<<<< HEAD
-  
-=======
->>>>>>> 227ebcb423522a01d2d358ca1c12e84a8ee4a8f7
 
   return axios
     .post('https://mentor-mee.herokuapp.com/auth/login', creds)
     .then(res => {
       dispatch({type: LOGIN_SUCCESS, payload: res.data.token});
       localStorage.setItem('token', res.data.token);
-<<<<<<< HEAD
-      localStorage.setItem('userId', res.data.userId);
-      localStorage.setItem('handle', creds.handle);
-=======
->>>>>>> 227ebcb423522a01d2d358ca1c12e84a8ee4a8f7
     })
     .catch(err => dispatch({type: LOGIN_FAILURE, payload: err}));
 };
